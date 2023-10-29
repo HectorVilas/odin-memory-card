8b7ee664938a1b08271d17327d5947b513195348
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-10-29 19:48:31 -0300

    import Header and Footer, use useState for scores and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
-
+  const [score, setScore] = useState(0)
+  const [highScore, setHighscore] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+  
   return (
-    <div>
+    <div id='app'>
+      <Header darkMode={darkMode} />
+      <div></div>
+      <Footer score={score} highScore={highScore} darkMode={darkMode} />
     </div>
   )
 }
