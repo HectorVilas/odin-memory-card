13ca8e14d849036e2d426c09e230b894dba89010
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-10-30 19:30:51 -0300

    import PlayArea component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import PlayArea from './components/PlayArea'
 import Footer from './components/Footer'
 
 export default function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighscore] = useState(0)
   const [darkMode, setDarkMode] = useState(false)
+  const [gameOver, setGameOver] = useState(false)
   
   return (
     <div id='app'>
       <Header darkMode={darkMode} />
-      <div></div>
+      <PlayArea gameOver={gameOver} setHighscore={setHighscore} setDarkMode={setDarkMode} setGameOver={setGameOver} />
       <Footer score={score} highScore={highScore} darkMode={darkMode} />
     </div>
   )
