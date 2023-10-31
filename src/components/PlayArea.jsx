4e684d2480ed99f075b133929aa56da6728f13d2
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-10-31 18:20:54 -0300

    move placeholder to parent, set quantity with cardsQuantity

--- src/components/PlayArea.jsx
+++ src/components/PlayArea.jsx
@@ -38,20 +38,11 @@ function CardsList(props) {
     }
   }
   
-  const Placeholder = [
-    {cardId: 0, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 1'},
-    {cardId: 1, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 2'},
-    {cardId: 2, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 3'},
-    {cardId: 3, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 4'},
-    {cardId: 4, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 5'},
-    {cardId: 5, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 6'},
-    {cardId: 6, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 7'},
-    {cardId: 7, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: 'card Title 8'},
-  ]
+
 
   return (
     <>
-      {Placeholder.sort(() => Math.random() - 0.5).map((item) =>
+      {props.cardsList.sort(() => Math.random() - 0.5).map((item) =>
         <Card
           key={item.cardId}
           cardId={item.cardId}
@@ -65,7 +56,13 @@ function CardsList(props) {
 }
 
 export default function PlayArea(props) {
-  const [cardsQuantity, setCardsQuantity] = useState(8)
+  const [cardsQuantity, setCardsQuantity] = useState(4)
+
+  const Placeholder = []
+
+  for (let i = 0; i < cardsQuantity; i++) {
+    Placeholder.push({cardId: i, imgUrl: 'https://i.imgur.com/ryWkZ8C.png', title: `card Title ${i}`})
+  }
 
   return (
     <div id="play-area">
@@ -75,6 +72,7 @@ export default function PlayArea(props) {
         : <CardsList
           setGameOver={props.setGameOver}
           cardsQuantity={cardsQuantity}
+          cardsList={Placeholder}
         />
       }
     </div>
