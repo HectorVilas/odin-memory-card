ad9381c58dfdbaabf3da09f950acff8ed4d1bea3
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-11-06 16:35:47 -0300

    reduce code repetition

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,7 @@ export default function Menu(props) {
   const isGameWon = props.chosenCards === props.cardsQuantity
   const isGameStarted = props.cardsQuantity === undefined
   const menuTitle = isGameStarted ? 'Welcome' : isGameWon ? 'You win' : 'You lose'
+  const newGameQuantity = [4, 8, 12, 16, 20, 24]
 
   return (
     <div id='game-menu'>
@@ -11,12 +12,11 @@ export default function Menu(props) {
       {!isGameStarted && <p>{`You got ${props.chosenCards} out of ${props.cardsQuantity} cards`}</p>}
       {!isGameStarted && <p>{`High score: ${props.highScore}`}</p>}
       <p>{isGameStarted ? 'How many cards?' : 'Play again?'}</p>
-      <button onClick={() => props.newGame(4)}>4 cards</button>
-      <button onClick={() => props.newGame(8)}>8 cards</button>
-      <button onClick={() => props.newGame(12)}>12 cards</button>
-      <button onClick={() => props.newGame(16)}>16 cards</button>
-      <button onClick={() => props.newGame(20)}>20 cards</button>
-      <button onClick={() => props.newGame(24)}>24 cards</button>
+      {
+        newGameQuantity.map((quantity) => {
+          return <button onClick={() => props.newGame(quantity)} key={quantity}>{quantity} cards</button>
+        })
+      }
     </div>
   )
 }
